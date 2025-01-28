65ea49b2431708b8c828fd081bd074f676b28f5d
maddy <maddy.siwick@icloud.com>
2025-01-28 06:27:23 -0500

    made the doctor model but it probably needs more work

--- Communicare/Filter/models.py
+++ Communicare/Filter/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 
 # Create your models here.
+
+class Doctor(models.Model):
+    name=models.CharField(max_length=100)
+    specialty=models.CharField(max_length=100)
+    accessibility=models.BooleanField(default=True)
+    sex=models.BooleanField(default=True) #probably shouldnt be a bool idk check before migrating
+    location=models.PointField(defauly=Point(0,0))
+
+    def __str__(self):
+        return self.name
