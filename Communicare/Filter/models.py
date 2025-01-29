cb89ea2433b04c2bde018fde0d794a803f66efb7
maddy <maddy.siwick@icloud.com>
2025-01-29 13:23:47 -0500

    decided to make a user model w doc and patient profiles for simplicity

--- Communicare/Filter/models.py
+++ Communicare/Filter/models.py
@@ -1,14 +1,22 @@
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-class Doctor(models.Model):
+#DONT MIGRATE ANYTHINGUNTIL YOU HAVE THE DB SET UP
+    
+class User(AbstractUser):
+    is_doctor=models.BooleanField(default=False)
+    is_patient=models.BooleanField(default=False)
     name=models.CharField(max_length=100)
-    specialty=models.CharField(max_length=100)
-    accessibility=models.BooleanField(default=True)
-    sex=models.BooleanField(default=True) #probably shouldnt be a bool idk check before migrating
-    location=models.PointField(defauly=Point(0,0))
+    acessibility=models.BooleanField(default=False)
+    location=models.PointField(default=Point(0,0))
 
     def __str__(self):
         return self.name
+
+class DoctorProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    is_male=models.BooleanField(default=False)
+    is_female=models.BooleanField(defaut=False)
+    specialty=models.CharField(max_length=100)
