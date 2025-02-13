947e6c0c2b800cf776cbdb795a8203ce69b9c7f2
maddy <maddy.siwick@icloud.com>
2025-02-13 13:59:06 -0500

    fuck offffff

--- Communicare/Communicare/settings.py
+++ Communicare/Communicare/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'Filer/static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'Filter/static')]
 
 MEDIA_URL = 'media/'
 
