48c1f939dac660d2f75b8ea94d53fbb27b81b96a
maddy <maddy.siwick@icloud.com>
2025-02-13 14:22:19 -0500

    pleaseeeee

--- Communicare/Communicare/settings.py
+++ Communicare/Communicare/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'Filter/static')]
 
@@ -133,7 +135,6 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #STATICFILES_DIRS = [
     #BASE_DIR/"static",
