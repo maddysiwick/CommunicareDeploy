15b3cb416bb06772052b02b75e456bd4de099a0a
EvaBatal9 <eva.batal2008@gmail.com>
2025-02-20 19:45:17 -0500

    ahhhhhh

--- Communicare/Communicare/urls.py
+++ Communicare/Communicare/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('logout/',authViews.LogoutView.as_view(),name='logout'),
     path('login',views.loginView,name='login'),
     path('triage', views.triage, name='triage'),
-    path('language-autocomplete',views.languageAutocomplete,name='language-autocomplete')
+    path('language-autocomplete',views.languageAutocomplete.as_view(),name='language-autocomplete')
 
 ]
 
