ee72cf6b15b88d254f63585b1e9d6670ae1556af
maddy <maddy.siwick@icloud.com>
2025-02-22 19:00:41 -0500

    i have having to do an entire push for ONE CHARACTER

--- Communicare/Filter/forms.py
+++ Communicare/Filter/forms.py
@@ -26,7 +26,7 @@ class PatientSignupForm(UserCreationForm):
     asylum=forms.BooleanField(required=False)
     class Meta(UserCreationForm.Meta):
         model=User
-        fields = ['language','name', 'accessibility', 'address']
+        fields = ['languages','name', 'accessibility', 'address']
 
     widgets = {
         'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'This is your name'}),
